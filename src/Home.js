ff2f2526a76b30dacaa80299bf4130cbbe404f8c
gowtham ggs <ggsgowtham17@gmail.com>
2019-11-22 18:41:26 +0530

    initial commit

---
+++ src/Home.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export const Home = () => (
+  <div class="container">
+  	<div class="row">
+  		<div class="col-md-4">
+  			hfdjshjdkhdkjshdjks
+  		</div>
+  		
+  		<div class="col-md-4">
+  			Test 1
+  		</div>
+
+  		<div class="col-md-4">
+  			Test 2
+  		</div>
+  	</div>
+  </div>
+)

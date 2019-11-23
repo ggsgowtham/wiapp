17f87b864822e2a08c1b0540f3b12cc4429c3935
gowtham ggs <ggsgowtham17@gmail.com>
2019-11-23 16:02:11 +0530

    added card and buttons

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { Contact } from './Contact';
 import { NoMatch } from './NoMatch';
 import { Layout } from './components/Layout';
 import { NavigationBar } from './components/NavigationBar';
-import { Jumbotron } from './components/Jumbotron';
+
 
 class App extends Component {
   render() {
@@ -14,10 +14,12 @@ class App extends Component {
       <React.Fragment>
         <Router>
           <NavigationBar />
-          <Jumbotron />
           <Layout>
             <Switch>
               <Route exact path="/" component={Home} />
+              <Route exact path="/about" component={About} />
+              <Route exact path="/contact" component={Contact} />
+              <Route exact path="/somewhere" component={Contact} />
               <Route component={NoMatch} />
             </Switch>
           </Layout>
--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,52 @@
-import React from 'react'
+import React from 'react';
+import { CardDeck, Card, Button} from 'react-bootstrap';
+import  image1 from './assets/download.jpg';
+import  image2 from './assets/download (1).jpg';
+import  image3 from './assets/download (2).jpg'
 
 export const Home = () => (
   <div class="container">
   	<div class="row">
   		<div class="col-md-4">
-  			hfdjshjdkhdkjshdjks
+  		<Card style={{ width: '20rem' }}>
+        <Card.Img variant="top" src={image1} />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the bulk of
+            the card's content.
+          </Card.Text>
+          <Button variant="primary" href="/somewhere">Go somewhere</Button>
+        </Card.Body>
+      </Card>
   		</div>
   		
   		<div class="col-md-4">
-  			Test 1
+  			<Card style={{ width: '20rem' }}>
+          <Card.Img variant="top" src={image2} />
+          <Card.Body>
+            <Card.Title>Card Title1</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the bulk of
+              the card's content.
+            </Card.Text>
+            <Button variant="primary" href="/somewhere">Go somewhere</Button>
+          </Card.Body>
+        </Card>
   		</div>
 
   		<div class="col-md-4">
-  			Test 2
+  			<Card style={{ width: '20rem' }}>
+          <Card.Img variant="top" src={image3} />
+          <Card.Body>
+            <Card.Title>Card Title2</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the bulk of
+              the card's content.
+            </Card.Text>
+            <Button variant="primary" href="/somewhere">Go somewhere</Button>
+          </Card.Body>
+        </Card>
   		</div>
   	</div>
   </div>
